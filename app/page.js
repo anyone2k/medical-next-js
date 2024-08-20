e6ce3d9e24c17621cb153a0f2f2639b86ac8d972
anyone2k <Salaheddineettounsi@gmail.com>
2024-08-20 06:15:07 -0400

    refactor: Add lazy loading for images on home page

--- app/page.js
+++ app/page.js
@@ -4,6 +4,10 @@ import { motion } from "framer-motion";
 import NavLink from "@/components/NavLink";
 import Video from "next-video";
 import getStarted from "/videos/vid.mp4";
+import Image from "next/image";
+import chatPic from "../images/chat.png";
+import treatmentPic from "../images/treatment.png";
+import globePic from "../images/globe.png";
 export default function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen bg-gradient-to-b from-[#28574E] to-[#1E232F] text-white">
@@ -54,31 +58,60 @@ export default function Home() {
       {/* Updated Section */}
       <section className="w-full py-12 bg-[#FFFFFF] text-[#28574E]">
         <div className="max-w-4xl mx-auto text-center px-4">
-          <h2 className="text-3xl font-bold mb-6">Experience Exceptional Healthcare In Canada With Omnicure Canada</h2>
+          <h2 className="text-3xl font-bold mb-6">
+            Experience Exceptional Healthcare In Canada With Omnicure Canada
+          </h2>
           <p className="text-lg sm:text-xl md:text-2xl mb-6">
-            Discover the pinnacle of healthcare services in Canada, where advancements in artificial intelligence (AI), quality care, cutting-edge research, expert doctors, and a commitment to patient success combine to provide an unparalleled medical tourism experience. Omnicure Canada utilizes AI to assist in diagnosing patients with remarkable accuracy, ensuring timely and personalized treatment plans. Connect with us today and embark on a journey of exceptional Canadian healthcare services powered by innovation.
+            Discover the pinnacle of healthcare services in Canada, where
+            advancements in artificial intelligence (AI), quality care,
+            cutting-edge research, expert doctors, and a commitment to patient
+            success combine to provide an unparalleled medical tourism
+            experience. Omnicure Canada utilizes AI to assist in diagnosing
+            patients with remarkable accuracy, ensuring timely and personalized
+            treatment plans. Connect with us today and embark on a journey of
+            exceptional Canadian healthcare services powered by innovation.
           </p>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
             <div className="flex flex-col items-center p-6 bg-[#F8F9FA] rounded-lg shadow-md">
-              <img src="./chat" alt="Second Opinion" className="w-12 h-12 mb-4" /> {/* Replace with your image path */}
+              <Image
+                src={chatPic}
+                alt="Second Opinion"
+                className="w-12 h-12 mb-4"
+              />
+
               <div className="text-xl font-semibold mb-2">Second Opinion</div>
               <p className="text-sm">
-                When facing a serious, life-changing illness, we understand the critical importance of obtaining expert advice, enhanced by AI-driven diagnostics.
+                When facing a serious, life-changing illness, we understand the
+                critical importance of obtaining expert advice, enhanced by
+                AI-driven diagnostics.
               </p>
             </div>
             <div className="flex flex-col items-center p-6 bg-[#F8F9FA] rounded-lg shadow-md">
-              <img src="./treatment.png" alt="Treatment" className="w-12 h-12 mb-4" /> {/* Replace with your image path */}
+              <Image
+                src={treatmentPic}
+                alt="Second Opinion"
+                className="w-12 h-12 mb-4"
+              />
               <div className="text-xl font-semibold mb-2">Treatment</div>
               <p className="text-sm">
-                Get help from the best hospitals and specialists that excel in providing premium healthcare directly from Canada, supported by AI-powered insights.
+                Get help from the best hospitals and specialists that excel in
+                providing premium healthcare directly from Canada, supported by
+                AI-powered insights.
               </p>
             </div>
             <div className="flex flex-col items-center p-6 bg-[#F8F9FA] rounded-lg shadow-md">
-              <img src="./globe.png" alt="Global Plans" className="w-12 h-12 mb-4" /> {/* Replace with your image path */}
+              <Image
+                src={globePic}
+                alt="Second Opinion"
+                className="w-12 h-12 mb-4"
+              />
+              {/* Replace with your image path */}
               <div className="text-xl font-semibold mb-2">Global Plans</div>
               <p className="text-sm">
-                Access comprehensive global healthcare plans that ensure your well-being no matter where you are, with the aid of advanced AI technologies.
+                Access comprehensive global healthcare plans that ensure your
+                well-being no matter where you are, with the aid of advanced AI
+                technologies.
               </p>
             </div>
           </div>
@@ -101,21 +134,30 @@ export default function Home() {
                 🏥
               </div>
               <h3 className="text-xl font-semibold">Top Hospitals</h3>
-              <p className="mt-2">Access the best medical facilities with state-of-the-art technology and AI-driven diagnostic tools.</p>
+              <p className="mt-2">
+                Access the best medical facilities with state-of-the-art
+                technology and AI-driven diagnostic tools.
+              </p>
             </div>
             <div className="flex flex-col items-center">
               <div className="w-16 h-16 mb-4 bg-[#28574E] text-white rounded-full flex items-center justify-center">
                 👩‍⚕️
               </div>
               <h3 className="text-xl font-semibold">Expert Doctors</h3>
-              <p className="mt-2">Connect with the most experienced and caring medical professionals, supported by cutting-edge AI.</p>
+              <p className="mt-2">
+                Connect with the most experienced and caring medical
+                professionals, supported by cutting-edge AI.
+              </p>
             </div>
             <div className="flex flex-col items-center">
               <div className="w-16 h-16 mb-4 bg-[#28574E] text-white rounded-full flex items-center justify-center">
                 💊
               </div>
               <h3 className="text-xl font-semibold">Comprehensive Care</h3>
-              <p className="mt-2">Receive all-inclusive care tailored to your needs, enhanced by AI-powered insights for better outcomes.</p>
+              <p className="mt-2">
+                Receive all-inclusive care tailored to your needs, enhanced by
+                AI-powered insights for better outcomes.
+              </p>
             </div>
           </div>
         </div>
